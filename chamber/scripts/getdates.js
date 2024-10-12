b3f9b3151128147dee27fe677d5aaab5a0927465
sadiebybee <byb22004@byui.edu>
2024-10-12 00:30:38 -0600

    chamberprojects

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,24 @@
+// ************ Create date and last modified date for the footer ************
+// Display the copyright year
+function getCopyrightYear() {
+    const year = new Date().getFullYear();
+    return `&copy; ${year}`;
+}
+document.getElementById("cYear").innerHTML = getCopyrightYear();
+
+// Display the last modified date of the page
+function getLastModified() {
+    const lastModified = new Date(document.lastModified).toGMTString();
+    return `Last Modified: ${lastModified}`;
+}
+document.getElementById("lastModified").innerHTML = getLastModified();
+
+// ********* Hamburger Menu *********
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
