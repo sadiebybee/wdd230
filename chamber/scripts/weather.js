1bbb8f538be35abcf9452d6cbc74a7c1c70ff06f
sadiebybee <byb22004@byui.edu>
2024-11-22 09:59:40 -0700

    sofar

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,35 +1,39 @@
-// The weather forecast functionality. Displaying the weather
-// forecast to the user 
-function weatherForecast() {
-  const lat = -25.74433236181061;
-  const long = 28.234333823643258;
-  const apiKey = 'f4619f75c2d45cc1bfe1a55992e82aaa';
+// Weather API 
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-  const currentTemp = document.querySelector('#current-temp');
-  const feelsLike = document.querySelector("#feels-like");
-  const windSpeed = document.querySelector("#wind-speed");
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.98&lon=-111.88&units=imperial&appid=ba21cf28c5bc872ed4f2778b5f85e93f';
 
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
+const feelsLike = document.querySelector("#feels-like");
+const windSpeed = document.querySelector("#wind-speed");
 
-  const apiFetch = async function () {
+
+async function apiFetcch() {
     try {
-      const response1 = await fetch(weatherUrl);
-      if (response1.ok) {
-        const weatherData = await response1.json();
-        displayResults(weatherData)
-      } else {
-        throw Error(await response1.text());
-      }
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // for testing only
+            displayResults(data); //uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  }
+}
 
-  function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src",iconsrc);
+    weatherIcon.setAttribute("alt","weather icon");
+    captionDesc.textContent =`${desc}`;
     feelsLike.innerHTML = `${data.main.feels_like.toFixed(0)}&deg;F`;
     windSpeed.innerHTML = `${data.wind.speed.toFixed(0)}mph`;
-  }
-  apiFetch();
+
 }
-weatherForecast();
+
+apiFetcch();
