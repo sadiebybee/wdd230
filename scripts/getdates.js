af15edc140925c17231ea1e92201ae5143179665
sadiebybee <byb22004@byui.edu>
2025-01-22 17:30:38 -0700

    hammenu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -20,3 +20,13 @@ function getLastModified() {
     return `Last Modified: ${lastModified}`;
 }
 document.getElementById("lastModified").innerHTML = getLastModified();
+
+// ********* Hamburger Menu *********
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
