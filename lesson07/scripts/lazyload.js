0156fbe82899cb1e28dd05bf6d1e3429e16bfc3a
sadiebybee <byb22004@byui.edu>
2025-02-20 12:53:48 -0700

    activities

---
+++ lesson07/scripts/lazyload.js
@@ -0,0 +1,14 @@
+// ************ Create date and last modified date for the footer ************
+// Display the copyright year
+function getCopyrightYear() {
+    const year = new Date().getFullYear();
+    return `&copy; ${year}`;
+}
+document.getElementById("cYear").innerHTML = getCopyrightYear();
+
+// Display the last modified date of the page
+function getLastModified() {
+    const lastModified = new Date(document.lastModified).toGMTString();
+    return `Last Modified: ${lastModified}`;
+}
+document.getElementById("lastModified").innerHTML = getLastModified();
